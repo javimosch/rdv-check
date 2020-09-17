bbcb0df9d6798093ee44955566f0d621a8c29790
Javier Leandro Arancibia <arancibiajav@gmail.com>
2020-09-17 11:55:34 +0200

    update

--- public/client.js
+++ public/client.js
@@ -2,6 +2,21 @@ new Vue({
     el:'.app',
     template:`
     <div ref="scope" class="scope">
+
+        <p>
+        In order to show the lack of availabilities for a meeting in the prefecture, I made this scraping script that checks the website every 3 minutes. Also, It can notify a subscribers list via email each time it doesn't found the word "existe plus"
+        </p>
+        <p>
+        Afin de montrer le manque de disponibilités pour un rendez-vous en préfecture, j'ai réalisé ce script de grattage qui vérifie le site toutes les 3 minutes. En outre, il peut notifier une liste d'abonnés par e-mail chaque fois qu'il ne trouve pas le mot «existe plus»
+        </p>
+        <p>
+        Para mostrar la falta de disponibilidad para una reunión en la prefectura, hice este script de raspado que revisa el sitio web cada 3 minutos. Además, puede notificar a una lista de suscriptores por correo electrónico cada vez que no encuentre la palabra "existe plus".
+        </p>
+
+        <p>
+        Este script es gratuito y es software libre.
+        </p>
+
         <label>Subscribe/Unsubscribe</label>
         <p>
             In order to receive emails (each time the script detects an availability), please subscribe.
@@ -47,6 +62,7 @@ new Vue({
             label{
                 text-align:left!important;
                 display:block;
+                font-weight: bold;
             }
         `
         this.$refs.scope.appendChild(style)
